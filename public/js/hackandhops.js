7347ed3825d12692685dee1db7157a0ea6bddfb7
Brett Whitham <brett@humenic.com>
2012-11-14 17:50:35 -0500

    added speaker pages. more styling. countdown to launch time.

--- public/js/hackandhops.js
+++ public/js/hackandhops.js
@@ -1,9 +1,28 @@
-$(document).ready(function(){
-
-	$('.feature').tinycarousel({ 
+$(document).ready(function() {
+    $('.feature').tinycarousel({ 
 		pager: true,
 		animation: true,
 		controls: false,
 		interval: false
 	});
-})
+
+	$(".speakerTile .moreButton").click(function() {
+        selectTile($(this).closest(".speakerTile").attr("speakerExpanded"));
+    });
+    $(".speakerExpanded .lessButton").click(function() {
+          unselectTile($(this).closest(".speakerExpanded"));
+    });
+});
+
+function selectTile(speakerExpanded) {
+    $(".speakersGrid").animate({opacity:0},400);
+
+        $("#" + speakerExpanded).css('opacity',1).css('z-index',103);
+
+};
+
+function unselectTile(speakerExpanded) {
+  var id = speakerExpanded.attr('id');
+    speakerExpanded.css('opacity',0).css('z-index',1);;
+           $(".speakersGrid").animate({opacity:1},400);	
+}
