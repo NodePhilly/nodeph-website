b924296bce7cc1aced1185662dacdac38909e9c5
tstrohecker <tedstrohecker@gmail.com>
2013-10-14 13:50:06 -0400

    added some bubbles stuff
    This is not the greatest code, but it works.

--- public/js/bubbles.js
+++ public/js/bubbles.js
@@ -1,16 +1,19 @@
 //Brett's bubbles
-//What makes a bubble a bubble
+//yes, this was written very quickly
+//yes, its not good code
+//it should not be practiced this way
+//you shouldnt be reading this anyway, because its freaking bubbles
 
 var Bubble = function(radius, acceleration, positionX, positionY){
 	var maxRadius = 10,
 		maxAcceleration = 1,
-		maxPositionX = 100,
+		maxPositionX = 95,
 		maxPositionY = 50;
 
 		this.radius = radius || Math.random() * maxRadius;
 		this.acceleration = acceleration || Math.random() * maxAcceleration;
 		this.positionX = positionX || Math.random() * maxPositionX;
-		this.positionY = positionY || Math.random() * maxPositionY  + 50;
+		this.positionY = positionY || Math.random() * maxPositionY  + 45;
 }
 Bubble.prototype.radius = 0;
 Bubble.prototype.acceleration = 0;
@@ -19,14 +22,19 @@ Bubble.prototype.positionY = 0;
 Bubble.prototype.bubblediv = {};
 
 Bubble.prototype.CreateAndAppendBubbleDiv = function(){
-	this.bubbleDiv = $('<div class="bubble"/>');
+	var bubble = this;
+	this.bubbleDiv = $('<div class="ZeBubbleHolder"><div class="bubble"/></div>');
+	
 	this.bubbleDiv.css({top: this.positionY + "%", left:this.positionX + "%"})
 	$('.bubblesContainer').append(this.bubbleDiv);
+	this.bubbleDiv.mouseenter(function(){
+		dodge(bubble);	
+	})
 }
 Bubble.prototype.MoveBubble = function(movingRight, alreadyMovingUp){
 	var bubble = this;
 	if(!alreadyMovingUp){
-		bubble.bubbleDiv.animate({top:"-10%"},{"queue":false, "easing":"linear","duration": Math.random() * 6000 + 2000, "complete": function(){
+		bubble.bubbleDiv.animate({top:"2%"},{"queue":false, "easing":"linear","duration": Math.random() * 6000 + 2000, "complete": function(){
 			bubble.bubbleDiv.remove();
 			bubble.CreateAndAppendBubbleDiv();
 			bubble.MoveBubble(Math.random() * 100 % 2 == 1 ? true : false, false);
@@ -48,8 +56,66 @@ Bubble.prototype.MoveBubble = function(movingRight, alreadyMovingUp){
 	}
 		
 }
+function dodge(bubble){
+	var negativeOrPositive = Math.random() * 100 % 2 == 1 ? -5: 5;
+	var left = bubble.bubbleDiv.position().left / bubble.bubbleDiv.parent().width() * 100 +negativeOrPositive ;
+	var top = bubble.bubbleDiv.css('top');
+	var percentTop = parseFloat(top);
+	bubble.bubbleDiv.stop();
+		bubble.bubbleDiv.animate({top:"2%"},{"queue":false, "easing":"linear","duration": percentTop < 200 ? 500: 1000 , "complete": function(){
+			bubble.bubbleDiv.remove();
+			bubble.CreateAndAppendBubbleDiv();
+			bubble.MoveBubble(Math.random() * 100 % 2 == 1 ? true : false, false);
+		}});
+	bubble.bubbleDiv.animate({left:left + "%"},{"easing":"linear","duration": 500});
+	bubble.MoveBubble(true, true);
+}
+
+
+function startDrinking(){
+	$(".whitespace").stop();
+	if($(".whitespace").height() > $(window).height() - 80){
+		startDrinking();	
+	}
+	var content = $("#content");
+
+	var blureffect = parseInt($(".whitespace").css('height'))/50 + "px";
+	$("p, h2, h1").css({'color': 'transparent',  'textShadow': 'black 0px 0px ' +blureffect})
+	if($(".whitespace").height() >= 400){
+	leftBlur.show();
+	rightBlur.show();
+	leftBlur.animate({ 'left':'+=6' , 'top' : content.offset().top, 'height' : content.height(), 'minHeight' : 0, 'color': 'transparent',  'textShadow': 'black 0px 0px ' +blureffect}, {'queue': false,"easing":"linear", "duration":300})
+	rightBlur.animate({'left': "-=6" , 'top' : content.offset().top, 'height' : content.height(), 'minHeight' : 0, 'color': 'transparent',  'textShadow': 'black 0px 0px ' +blureffect}, {'queue': false,"easing":"linear", "duration":300})
+	}
+	$(".whitespace").animate({'height': '+=40'},startDrinking);
+
+}
+function stopDrinking(){
+	$(".whitespace").stop();
+	var content = $("#content");
+
+	if(parseInt($(".whitespace").css('height')) <= 0){
+		leftBlur.css({'position':'absolute', 'left': content.offset().left , 'top' : content.offset().top, 'height' : content.height(), 'minHeight' : 0})
+		rightBlur.css({'position':'absolute', 'left': content.offset().left , 'top' : content.offset().top, 'height' : content.height(), 'minHeight' : 0})
+		leftBlur.hide();
+		rightBlur.hide();
+		return;
+	}	
+
+	var blureffect = parseInt($(".whitespace").css('height'))/50 + "px";
+	$("p, h2, h1").css({'color': 'transparent',  'textShadow': 'black 0px 0px ' +blureffect})
+	if($(".whitespace").height() > 400){
+		leftBlur.show();
+		rightBlur.show();
+		leftBlur.animate({ 'left':'-=6' , 'top' : content.offset().top, 'height' : content.height(), 'minHeight' : 0, 'color': 'transparent',  'textShadow': 'black 0px 0px ' +blureffect}, {'queue': false,"easing":"linear", "duration":300})
+		rightBlur.animate({'left': "+=6" , 'top' : content.offset().top, 'height' : content.height(), 'minHeight' : 0, 'color': 'transparent',  'textShadow': 'black 0px 0px ' +blureffect}, {'queue': false,"easing":"linear", "duration":300})
+	}
+	$(".whitespace").animate({'height': '-=40'},{'queue': true,"easing":"linear", "duration":300,"complete": stopDrinking})
+}
 
 var testBubbles = [];
+var leftBlur = {};
+var rightBlur = {};
 $(document).ready(function(){
 
 	for(var i = 0; i < 20; i++){
@@ -58,5 +124,19 @@ $(document).ready(function(){
 		testBubbles[i].MoveBubble(i % 2 == 1 ? true : false, false);
 
 	}
+	var content = $("#content");
 
+	leftBlur = content.clone();
+	rightBlur = content.clone();
+	leftBlur.css({'position':'absolute', 'left': content.offset().left , 'top' : content.offset().top, 'height' : content.height(), 'minHeight' : 0, 'display': 'none'})
+	rightBlur.css({'position':'absolute', 'left': content.offset().left , 'top' : content.offset().top, 'height' : content.height(), 'minHeight' : 0, 'display':'none'})
+	$("body").append(leftBlur);
+	$("body").append(rightBlur);
+
+	$("header").mousedown(startDrinking);
+	$("body").mouseup(stopDrinking);
+	$(window).resize(function(){
+		leftBlur.css({'position':'absolute', 'left': content.offset().left , 'top' : content.offset().top, 'height' : content.height(), 'minHeight' : 0})
+		rightBlur.css({'position':'absolute', 'left': content.offset().left , 'top' : content.offset().top, 'height' : content.height(), 'minHeight' : 0})
+	});
 });
